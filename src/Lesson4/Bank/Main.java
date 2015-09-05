3ed6a3a110b338eb122a918bdb4f45ca787a85d6
freedomserg <freedomserg97@gmail.com>
2015-09-06 00:36:46 +0300

    Commit Lesson4

--- src/Lesson4/Bank/Main.java
+++ src/Lesson4/Bank/Main.java
@@ -5,22 +5,22 @@ package Lesson4.Bank;
  */
 public class Main{
     public static void main(String[] args) {
-        BankOperations operation_one = new ActiveOperations();
-        operation_one.setType("Investment loan");
-        operation_one.setClient("legal entity");
-        System.out.print("Type of the operation: " + operation_one.getType() + ";" + " ");
-        System.out.println("client: " + operation_one.getClient());
+        BankOperations operationOne = new ActiveOperations();
+        operationOne.setType("Investment loan");
+        operationOne.setClient("legal entity");
+        System.out.print("Type of the operation: " + operationOne.getType() + ";" + " ");
+        System.out.println("client: " + operationOne.getClient());
 
-        BankOperations operation_two = new PassiveOperations();
-        operation_two.setType("Short-term deposit");
-        operation_two.setClient("individual");
-        System.out.print("Type of the operation: " + operation_two.getType() + ";" + " ");
-        System.out.println("client: " + operation_two.getClient());
+        BankOperations operationTwo = new PassiveOperations();
+        operationTwo.setType("Short-term deposit");
+        operationTwo.setClient("individual");
+        System.out.print("Type of the operation: " + operationTwo.getType() + ";" + " ");
+        System.out.println("client: " + operationTwo.getClient());
 
-        BankOperations operation_three = new OtherOperations();
-        operation_three.setType("Payment card");
-        operation_three.setClient("individual");
-        System.out.print("Type of the operation: " + operation_three.getType() + ";" + " ");
-        System.out.println("client: " + operation_three.getClient());
+        BankOperations operationThree = new OtherOperations();
+        operationThree.setType("Payment card");
+        operationThree.setClient("individual");
+        System.out.print("Type of the operation: " + operationThree.getType() + ";" + " ");
+        System.out.println("client: " + operationThree.getClient());
     }
 }
