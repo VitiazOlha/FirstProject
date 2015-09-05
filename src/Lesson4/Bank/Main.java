784299ba58d26ed7abe1ada3edbd59be1d533832
freedomserg <freedomserg97@gmail.com>
2015-09-06 00:25:32 +0300

    Commit Lesson4

--- src/Lesson4/Bank/Main.java
+++ src/Lesson4/Bank/Main.java
@@ -7,9 +7,20 @@ public class Main{
     public static void main(String[] args) {
         BankOperations operation_one = new ActiveOperations();
         operation_one.setType("Investment loan");
-        System.out.println(operation_one.getType());
+        operation_one.setClient("legal entity");
+        System.out.print("Type of the operation: " + operation_one.getType() + ";" + " ");
+        System.out.println("client: " + operation_one.getClient());
 
+        BankOperations operation_two = new PassiveOperations();
+        operation_two.setType("Short-term deposit");
+        operation_two.setClient("individual");
+        System.out.print("Type of the operation: " + operation_two.getType() + ";" + " ");
+        System.out.println("client: " + operation_two.getClient());
 
-
+        BankOperations operation_three = new OtherOperations();
+        operation_three.setType("Payment card");
+        operation_three.setClient("individual");
+        System.out.print("Type of the operation: " + operation_three.getType() + ";" + " ");
+        System.out.println("client: " + operation_three.getClient());
     }
 }
